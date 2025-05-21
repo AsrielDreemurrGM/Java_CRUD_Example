e84dbed4cb273c1d0953e9a06b96eab616ff7e76
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-05-21 00:10:09 -0300

    Created DAO Interface And In-Memory Implementation
    - Created The IClientDAO Interface In The br.com.eaugusto.dao Package;
    - Declared Methods For Registering, Deleting, Modifying, And Searching Clients;
    - Created The ClientMapDAO Class With An In-Memory HashMap Implementation;
    - Handled All CRUD Operations Using CPF As The Unique Key;

---
+++ Java_CRUD_Example/src/br/com/eaugusto/dao/IClientDAO.java
@@ -0,0 +1,19 @@
+package br.com.eaugusto.dao;
+
+import java.util.Collection;
+
+import br.com.eaugusto.domain.Client;
+
+public interface IClientDAO {
+	
+	public Boolean register(Client client);
+	
+	public void delete(String cpf);
+	
+	public void modify(Client client);
+	
+	public Client search(String cpf);
+	
+	public Collection<Client> searchAll();
+
+}
