9db6ba0f09b81a39e4f08a4860b92a575b36c45f
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-05-21 00:54:14 -0300

    Implemented Client HashSet DAO For Client Storage
    - Created ClientSetDAO Class That Implements IClientDAO Interface;
    - Used HashSet To Store Unique Client Objects Based On Their CPF;
    - Implemented Methods To Register, Delete, Modify, Search, And Retrieve All Clients;
    - Learned Differences Between HashSet And HashMap Storage;
    - Ensured Consistent Interface And Behavior With ClientMapDAO;

---
+++ Java_CRUD_Example/src/br/com/eaugusto/dao/ClientSetDAO.java
@@ -0,0 +1,58 @@
+package br.com.eaugusto.dao;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import br.com.eaugusto.domain.Client;
+
+public class ClientSetDAO implements IClientDAO {
+
+	private Set<Client> set;
+	
+	public ClientSetDAO() {
+		this.set = new HashSet<>();
+	}
+	
+	@Override
+	public Boolean register(Client client) {
+		return set.add(client);
+	}
+
+	@Override
+	public void delete(String cpf) {
+		Client toRemove = search(cpf);
+		if (toRemove != null) {
+			set.remove(toRemove);
+		}
+	}
+
+	@Override
+	public void modify(Client client) {
+		Client existingClient = search(client.getCpf());
+		
+		if (existingClient != null) {
+			existingClient.setName(client.getName());
+			existingClient.setPhoneNumber(client.getPhoneNumber());
+			existingClient.setAddress(client.getAddress());
+			existingClient.setAddressNumber(client.getAddressNumber());
+			existingClient.setCity(client.getCity());
+			existingClient.setState(client.getState());
+		}
+	}
+
+	@Override
+	public Client search(String cpf) {
+		for (Client client : set) {
+			if (client.getCpf().equals(cpf));
+			return client;
+		}
+		return null;
+	}
+
+	@Override
+	public Collection<Client> searchAll() {
+		return set;
+	}
+
+}
