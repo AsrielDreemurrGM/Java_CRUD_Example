a2a1a45d86e7a40a9108effe01f2b5a29ea42d86
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-06-06 01:18:44 -0300

    Updated GenericMapDAO Javadocs

--- Java_CRUD_Example/src/br/com/eaugusto/dao/generic/GenericMapDAO.java
+++ Java_CRUD_Example/src/br/com/eaugusto/dao/generic/GenericMapDAO.java
@@ -7,22 +7,45 @@ import java.util.Map;
 import br.com.eaugusto.domain.Persistable;
 
 /**
- * Abstract implementation of a generic DAO using nested Maps for storage.
+ * Abstract Generic DAO Implementation Using Nested Maps As Storage.
+ * 
+ * <p>This class provides basic CRUD operations for entities of type {@code T}
+ * that implement {@link Persistable}. It manages a two-level Map:
+ * an outer Map from Class<T> to inner Maps, and inner Maps from unique ID strings to entities.</p>
+ * 
+ * <p>Subclasses must specify the entity Class type and
+ * implement how to update an existing registered entity with new data.</p>
  *
- * @param <T> The type of entity.
+ * @param <T> The type of persistable entity managed by this DAO.
  * 
  * @author Eduardo Augusto (https://github.com/AsrielDreemurrGM/)
  * @since June 02, 2025
  */
 public abstract class GenericMapDAO<T extends Persistable> implements IGenericDAO<T> {
 
-	// Outer map: associates each class with its corresponding map of entities
+	/** 
+	 * Storage map: associates each entity class with a map of entities by their ID.
+	 */
 	protected Map<Class<T>, Map<String, T>> storage;
 
+	/**
+	 * Returns the class object representing the entity type managed.
+	 * 
+	 * @return Class of type T
+	 */
 	public abstract Class<T> getClassType();
 
+	/**
+	 * Updates the data of a registered entity with values from a new entity.
+	 * 
+	 * @param newEntity The entity containing new data.
+	 * @param registeredEntity The currently registered entity to update.
+	 */
 	public abstract void updateRegisteredEntityWithNewData(T newEntity, T registeredEntity);
 
+	/**
+	 * Constructor initializes the storage map and ensures inner map for the entity class exists.
+	 */
 	protected GenericMapDAO() {
 		this.storage = new HashMap<>();
 		storage.computeIfAbsent(getClassType(), entityClass -> new HashMap<>());
